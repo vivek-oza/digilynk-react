74ed7497960fc252764f792f3a5804be18712255
vivek-oza <112532672+vivek-oza@users.noreply.github.com>
2025-04-19 17:40:42 +0530

    Latest 3

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -20,7 +20,9 @@ export default function Home() {
       <WhyChooseUs></WhyChooseUs>
 
       {/* Services section */}
-      <ServicesSection />
+      <div id="services">
+        <ServicesSection />
+      </div>
 
       {/* Features scroll section */}
       <FeaturesMarquee></FeaturesMarquee>
